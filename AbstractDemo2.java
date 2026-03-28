90da194216062d831365f15519e261dc325ebb3b
sakshi Jagtap <sakshijagtap205@gmail.com>
2026-03-28 14:09:22 +0530

    Add files via upload
    # Java Abstract Class Example
    
    ## Overview
    This project demonstrates the use of abstract classes in Java. It shows how a base abstract class contains both concrete and abstract methods, and how a derived class implements the abstract method.
    
    ## Concepts Used
    - Abstract Class
    - Inheritance
    - Method Overriding
    
    ##  Code Structure
    
    ### Base Class
    - Contains:
      - A concrete method: Addition()
      - An abstract method: substraction()
    
    ### Derived Class
    - Inherits from Base class
    - Implements the abstract method substraction()
    
    ##  How to Run
    
    1. Save the file as `Main.java`
    2. Compile the code:

---
+++ AbstractDemo2.java
@@ -0,0 +1,15 @@
+abstract class Base
+{
+    public int i,j;
+    public int Addition(int a , int b)  //concrete
+    {
+        return a+b;
+    }
+    abstract int substraction (int a , int b);   //abstract
+
+}
+class Derived extends Base    //Error
+{
+
+  nj
+}

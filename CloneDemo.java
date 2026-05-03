9f0c78eba5ceaadfb6c0160c4959ee5aa1eca3d1
sakshi Jagtap <sakshijagtap205@gmail.com>
2026-05-03 11:04:09 +0530

    Add files via upload

---
+++ CloneDemo.java
@@ -0,0 +1,39 @@
+class Demo implements Cloneable
+{
+    public int i;
+    public int j;
+
+    public Demo (int a , int b)
+    {
+        this.i = a;
+        this.j = b;
+    }
+
+    public Object clone() throws CloneNotSupportedException
+    {
+        return super.clone();
+    }
+}
+class CloneDemo
+{
+    public static void main(String A[])
+    {
+        try
+        {
+
+        
+            Demo dobj1 = new Demo(11,21);
+            Demo dobj2 = (Demo)dobj1.Clone();
+
+            System.out.println(dobj2.i);
+            System.out.println(dobj2.j);
+        }
+
+        
+            catch(CloneNotSupportedException cobj)
+            {}
+        
+
+        
+    }
+}

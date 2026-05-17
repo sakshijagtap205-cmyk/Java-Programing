b79d299b4b9e7d10c845ee11218c32a6a238a6fb
sakshi Jagtap <sakshijagtap205@gmail.com>
2026-05-17 23:57:09 +0530

    Add files via upload

---
+++ EventHandlingSeprateClass.java
@@ -0,0 +1,28 @@
+import javax.swing.*;
+import java.awt.event.*;
+class MarvellousListner implements ActionListner
+{
+    public void ActionPerformed(ActionEvent aobj)
+  {
+     system.out.println("Button Clicked...");
+  }
+}
+
+class EventHandlingSeprateClass
+{
+    public static void main(String A[])
+    {
+        JFrame fobj = new JFrame ("Ppa");
+        JButton bobj =new JButton("OK");
+        bobj.setBounds(100 , 100, 150,50);
+        bobj.addActionListner(new MarvellousListner());
+
+        fobj.add(bobj);
+
+        fobj.setSize(500 , 400);
+        fobj.setLayout(null);
+        fobj.setVisible(true);
+
+        fobj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }   
+}      

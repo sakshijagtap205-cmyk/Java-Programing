f0939398956f59663ef58fddaa1febdfaed569f8
sakshi Jagtap <sakshijagtap205@gmail.com>
2026-05-23 23:43:32 +0530

    Add files via upload

---
+++ Exception1.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class ExceptionDemo1
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        int iNo1=0 , iNo2=0, iAns=0;
+
+        System.out.println("Enter First  number:");
+        iNo1 = sobj.nextInt();
+
+        System.out.println("Enter Second  number:");
+        iNo2 = sobj.nextInt();
+
+        iAns =iNo1 / iNo2;
+
+        System.out.println("Division is :"+iAns);
+    }
+}

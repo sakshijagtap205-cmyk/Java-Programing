dff120d1938c97c23bcdd636f6b1389d4298fb35
sakshi Jagtap <sakshijagtap205@gmail.com>
2026-06-01 22:07:40 +0530

    Add files via upload

---
+++ Exception5.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+class Exception5
+
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        int iNo1=0 , iNo2=0, iAns=0;
+
+        System.out.println("Enter First  number:");
+        iNo1 = sobj.nextInt();
+    
+        System.out.println("Enter Second  number:");
+        iNo2 = sobj.nextInt();
+
+    try
+        
+     {
+        System.out.println("Inside try block");
+        iAns = iNo1 / iNo2;
+     }
+     catch(ArithmeticException aobj)                //specific catch
+
+     {
+       System.out.println("Inside catch block");  
+       System.out.println(aobj);  
+     }
+    catch(Exception eobj)                               //Generic catch
+    {
+        System.out.println("Inside Generic catch"); 
+    }
+        System.out.println("Division is :"+iAns);
+    }
+}

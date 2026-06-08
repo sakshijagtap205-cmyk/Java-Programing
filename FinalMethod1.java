8d3d429a678d29197911a4eb03fbe130a4bd9840
sakshi Jagtap <sakshijagtap205@gmail.com>
2026-06-08 11:20:20 +0530

    Add files via upload

---
+++ FinalMethod1.java
@@ -0,0 +1,25 @@
+class base 
+{
+    public void fun()
+    {}
+    public void gun()
+    {}
+
+}
+
+class Derived extends base 
+{
+    public void run()
+    {}
+    publi8c void gun()
+    {}
+    
+}
+
+class FinalMethod1
+{
+    public static void main()
+    {
+        Demo 
+    }
+}

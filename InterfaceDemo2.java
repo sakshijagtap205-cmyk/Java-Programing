b215738bbb82f69a90d1fbc311a2c21bb3433bcc
sakshi Jagtap <sakshijagtap205@gmail.com>
2026-06-16 09:47:09 +0530

    Add files via upload

---
+++ InterfaceDemo2.java
@@ -0,0 +1,20 @@
+
+interface Demo
+{
+    int no =11;
+    void Display();
+
+}
+abstract class Hello implements Demo 
+{
+    
+}
+
+
+class interfaceDemo2
+{
+    public static void main(String A[])
+    {
+        Hello hobj = new Hello();//error
+    }
+}

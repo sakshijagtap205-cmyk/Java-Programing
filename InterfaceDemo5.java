ad65689a0cf93c4c8b3138bbd98e7490a5a2ce2d
sakshi Jagtap <sakshijagtap205@gmail.com>
2026-06-19 11:03:20 +0530

    Add files via upload

---
+++ InterfaceDemo5.java
@@ -0,0 +1,26 @@
+interface A
+{
+    void fun();
+}
+
+interface B
+{
+    void fun();
+}
+
+class Demo implements  A , B  //multiple inheritance
+{
+    public void fun()
+    {
+        System.out.println("inside fun");
+    }
+    public void gun();
+    {}
+}
+class interfaceDemo3
+{
+    public static void main(String A[])
+    {
+        Demo.dobj
+    }
+}

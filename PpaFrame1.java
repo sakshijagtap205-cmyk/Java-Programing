c9f9be7b0d5345f1aee67af2b2f8e67a05eb582f
sakshi Jagtap <sakshijagtap205@gmail.com>
2026-06-27 11:37:50 +0530

    Add files via upload

---
+++ PpaFrame1.java
@@ -0,0 +1,12 @@
+import java.awt.*;
+
+class PpaFrame1
+{
+    public static void main(String A[])
+    {
+        Frame fobj = new Frame ("Ppa");
+
+        fobj.setSize(300 , 300);
+        fobj.setVisible(true);
+    }
+}

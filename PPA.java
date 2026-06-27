2ddbada1d3cc61cfbceba90d8cc3330bcfd8ac46
sakshi Jagtap <sakshijagtap205@gmail.com>
2026-06-27 11:37:19 +0530

    Add files via upload

---
+++ PPA.java
@@ -0,0 +1,18 @@
+import Marvolous.Hello;
+import MArvolous.Demox;
+import Marvolous.Infosystems.LB;
+
+class PPA
+{
+
+    public static void main (String A[])
+    {
+        Hello hobj = new Hello();
+        Demox dobj = new Demox();
+        LB lobj = new LB();
+
+        hobj.fun();
+        dobj.gun();
+        lobj.sun();
+    }
+}

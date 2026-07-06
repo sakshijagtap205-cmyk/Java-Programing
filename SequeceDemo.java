1f1373407713c93fe02a02de1c1b877da6c8525a
sakshi Jagtap <sakshijagtap205@gmail.com>
2026-07-06 15:36:23 +0530

    Add files via upload

---
+++ SequeceDemo.java
@@ -0,0 +1,20 @@
+Class SequenceDemo
+{
+  Public Static void main (string A[])
+ {
+    int iNo1 =  10;
+    int iNo2 =  11;
+    int iAns =  0;
+    iAns = iNo1 * iNo2;
+
+    System.out.Println(iAns);
+
+
+
+
+ }
+
+
+
+
+}

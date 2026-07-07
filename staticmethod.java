a4615fca64d9967af9bbc133faafb2d5973d4502
sakshi Jagtap <sakshijagtap205@gmail.com>
2026-07-07 13:51:37 +0530

    Add files via upload

---
+++ staticmethod.java
@@ -0,0 +1,38 @@
+class Demo
+{
+    public int i;
+    static public int j=21;
+    
+    public Demo()
+    {
+        this.i=11;
+    }
+    
+    
+    public void fun()  //Instance method
+    {
+        System.out.println("fun i:"+this.i);
+        System.out.println("fun i:"+Demo.j);
+        
+    
+    }
+    public void gun()  //Instance method
+    {
+       // System.out.println("gun i:"+this.i);  //not allowed
+        System.out.println("gun i:"+Demo.j);
+        
+    }
+}
+class staticmethod
+{
+    public static void main(string A[])
+    {
+        Demo.gun();
+        System.out.println(Demo.j);
+        Demo dobj =  new Demo();
+        System.out.println(dobj.i);
+        dobj.fun();
+
+
+    }
+}
